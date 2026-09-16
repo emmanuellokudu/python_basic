7cf15e46435ff6b1ba14109bb93a52ee11086819
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-16 19:09:28 +0300

    introductory program initilization

---
+++ python_basic.py
@@ -0,0 +1,21 @@
+name = "Emmanuel Lokudu"
+age = 21
+favorite_language = "Python"
+goal = "To become a proficient Python developer"
+
+print(name)
+print(age)
+print(favorite_language)
+print(goal)
+
+#the above code defines variables for name, age, favorite programming language, and a personal goal. It then prints each of these variables to the console.
+
+
+name = "Emmanuel Lokudu"
+role = "Software Developer"
+language = "Python"
+
+print("Hello!")
+print("My name is", name)
+print("I am an", role)
+print("I am learning", language)
