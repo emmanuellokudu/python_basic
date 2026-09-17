d5b3e06ebd3ca2eabc4edbfc38b253c292779358
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-17 09:45:50 +0300

    code refactor

--- python_basic.py
+++ python_basic.py
@@ -3,10 +3,10 @@ age = 21
 favorite_language = "Python"
 goal = "To become a proficient Python developer"
 
-print(name)
-print(age)
-print(favorite_language)
-print(goal)
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"My favorite programming language is {favorite_language}.")
+print(f"My goal is {goal}.")
 
 #the above code defines variables for name, age, favorite programming language, and a personal goal. It then prints each of these variables to the console.
 
@@ -15,7 +15,4 @@ name = "Emmanuel Lokudu"
 role = "Software Developer"
 language = "Python"
 
-print("Hello!")
-print("My name is", name)
-print("I am an", role)
-print("I am learning", language)
+print(f"My name is " + name + ", I am a " + role + " and my favorite programming language is " + language)
